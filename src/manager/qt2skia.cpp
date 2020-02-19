c99f70cef6280b43bb7818b18d9197ec51a6c63e
Martin <martinreininger@gmx.net>
2020-02-19 21:25:54 +0100

    first skia implementation + fill image perfromance rendering

--- src/manager/qt2skia.cpp
+++ src/manager/qt2skia.cpp
@@ -18,6 +18,8 @@
    with this program; if not, write to the Free Software Foundation, Inc.,
    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+   Base on https://github.com/telishev/sneakPic/blob/master/src/renderer/qt2skia.cpp
+
 **************************************************************************************/
 
 #include "qt2skia.h"
